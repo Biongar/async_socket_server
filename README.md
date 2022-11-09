1d212e3041b29db84fa1e2aa771d6c1092120faf
Biongar <mixailis5050@gmail.com>
2022-11-09 11:41:31 +0300

    Reworked client.py from func to class

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
->
+> 
 
 ## Create virtual environment
 
--- client.py
+++ client.py
@@ -2,28 +2,34 @@ import socket
 import threading
 
 
-''' TODO class Client: ... '''
+class Client:
+    def __init__(self, host: str = '127.0.0.1', port: int = 8000):  
+        self.host = host
+        self.port = port
+        
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST = '127.0.0.1'
-PORT = 8000
+    def connect_to_server(self):
+        self.client_socket.connect((self.host, self.port))
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+    def get_message(self):
+        while True:
+            message = self.client_socket.recv(1024).decode()
+            print(message)
 
-def get_message():
-    while True:
-        message = client_socket.recv(1024).decode()
-        print(message)
+    def send_message(self):
+        while True:
+            message = input().encode()
+            self.client_socket.send(message)
 
-def send_message():
-    while True:
-        message = input().encode()
-        client_socket.send(message)
-
-
-def main():
-    threading.Thread(target=get_message).start()
-    send_message()
+    def event_loop(self):
+        threading.Thread(target=self.get_message).start()
+        self.send_message()
+        
+    def run(self):
+        self.connect_to_server()
+        self.event_loop()
     
 if __name__ == '__main__':
-    main()
+    client = Client()
+    client.run()
